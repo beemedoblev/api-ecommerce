97ed0be0a4d5d23be17b8e8126e82cd12138decd
beemedoblev <tomi.wiersch@gmail.com>
2021-12-01 12:56:17 -0300

    subiendo archivos de rutas y controladores / modelos

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Dbconnection exitosa");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//  app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+//  app.use("/api/products", productRoute);
+//  app.use("/api/carts", cartRoute);
+//  app.use("/api/orders", orderRoute);
+//  app.use("/api/checkout", stripeRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("backend corriendo");
+});
